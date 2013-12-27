a0edf0bdacd6d7f9fd447ce1f5c724d14d40eba9
Dow Drake <dowdrake@msn.com>
2013-12-27 08:35:57 -0800

    Broke up test and added tests for string and equality

--- PentominoTests/BoardTests.cs
+++ PentominoTests/BoardTests.cs
@@ -13,10 +13,37 @@ public class BoardTests
         Board board = new Board(10, 6);
         Moose moose = new Moose();
         Placement[] placements = board.PossiblePlacementsFor(moose);
-        Assert.Equal(3*9*4 + 5*7*4, placements.Length);
-        Assert.Equal<string>("Moose, Facing left at (0,0)", placements[0].ToString());
+        Assert.Equal(3 * 9 * 4 + 5 * 7 * 4, placements.Length);
     }
 
+    [Fact]
+    public void MoosePlacementsCanDisplayAsStrings()
+    {
+        //var mock = new Mock<Game>();
+        //mock.Setup(game => game.FreePieces()).Returns(new IPiece[0]);
+
+        Board board = new Board(10, 6);
+        Moose moose = new Moose();
+        Placement[] placements = board.PossiblePlacementsFor(moose);
+        Assert.Equal<string>("Moose, Facing left at (0,0)", placements[0].ToString());
+    }
+}
+
+public class PieceTests
+{
+    [Fact]
+    public void PieceShapesCanDisplayAsString()
+    {
+        Moose moose = new Moose();
+        Assert.Equal<string>("Moose, Facing left", moose.Shapes[0].ToString());
+    }
+
+    [Fact]
+    public void PieceShapesAreEqualIfTheirValuesAre()
+    {
+        Moose moose = new Moose();
+        Assert.True(moose.Equals(new Moose()));
+    }
 }
 
 
